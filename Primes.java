20e6bd36f41402a9805108554b3e67bd4151a2af
shirgnapp <gnapp3@gmail.com>
2024-12-01 22:06:27 +0200

    done

--- Primes.java
+++ Primes.java
@@ -1,5 +1,35 @@
 public class Primes {
     public static void main(String[] args) {
-        // Replace this statement with your code
+        int n= Integer.parseInt(args[0]);
+        boolean[] Prime = new boolean[n];
+        for(int j=2; j<Prime.length; j++ )
+        {
+            Prime[j] = true;
+        }
+        int i=2;
+        while (i < Math.sqrt(n)){ 
+            for(int s=0; s<n; s++){
+                if (s % i == 0) {
+                    Prime[s] = false;
+                }
+            }
+                if ( i ==2 ) 
+                    i++;
+                
+                else
+                i+=2;
+            }
+            Prime[2] = true;
+            int total = 0;
+            for(int t = 0; t<n; t++) {
+                if (Prime[t] == true) {
+                    System.out.println(t);
+                    total++;
+                }
+            }
+            int p = 0;
+            p = (total * 100) / n;
+            System.out.println("There are " + total + " primes between 2 and " + n + " ( " + p + "% are primes)");
+        }
+        
     }
-}
