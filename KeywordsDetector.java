a53c3636860c7d5c1526ac090710ad1729c9feda
shirgnapp <gnapp3@gmail.com>
2024-12-03 19:42:40 +0200

    done

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -1,17 +1,7 @@
 public class KeywordsDetector {
     public static void main(String[] args) {
         String[] sentences = {
-            "Our product will transform the market",
-            "Programming is both painful and engaging",
-            "This has nothing to do with machine learning",
-            "We need to leverage our core competencies",
-            "Let's talk about data and algorithms",
-            "Chatbots are great but must be used carefully",
-            "This blockchain-based solution will disrupt the industry",
-            "The team showed great Synergy in the last project",
-            "Use simple words without hype and fluff",
-            "Our new technology presents a significant paradigm shift",
-            "Effective presentations must be clear, concise, and humble"
+            "Our product will transform the market"
         };
         // Some keywords that typically signal bullshit contents in business presentations 
         String[] keywords = {"synergy", "disrupt", "leverage", "Paradigm", "transform"};
@@ -21,6 +11,17 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        String SentenceByIndewx = "";
+       for(int i =0; i<sentences.length; i++)  { 
+         for(int j=0; j < keywords.length; j++){ 
+            SentenceByIndewx = sentences[i];
+            String[] words = SentenceByIndewx.split(" ");
+                for(int a=0; a<words.length; a++){
+                    if (words[a].equals(keywords[j])) {
+                    System.out.println(keywords[j]);
+                     }
+                }
+            }
+        }
     }
 }
